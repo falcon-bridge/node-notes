4a91f5963573cb9ea5ccf34a31efc43e6e2e8d0c
Aayush <falcon.bridge2000@gmail.com>
2022-02-13 05:21:36 +0530

    got user input from CLI using command ine arguments

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const chalk = require("chalk");
 const getNotes = require("./notes");
 
 // console.log(getNotes());
-console.log(chalk.green("Success"));
+console.log(chalk.blue.inverse("Success"));
 
 // console.log(validator.isEmail("kumartest.com"));
 // console.log(validator.isURL("kumartestcom"));
+
+// console.log(process.argv);
+console.log(process.argv[2]);
