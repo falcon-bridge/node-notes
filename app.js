36f8752a42714a7fc44a8f32a2d6f43aba6ca4fc
Aayush <falcon.bridge2000@gmail.com>
2022-02-13 05:31:58 +0530

    added functionality to detect some special arguments and do something accordingly

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// const validator = require("validator");
 const chalk = require("chalk");
 
 const getNotes = require("./notes");
 
-// console.log(getNotes());
-console.log(chalk.blue.inverse("Success"));
+const command = process.argv[2];
 
-// console.log(validator.isEmail("kumartest.com"));
-// console.log(validator.isURL("kumartestcom"));
-
-// console.log(process.argv);
-console.log(process.argv[2]);
+if (command === "add") {
+  console.log("adding note");
+} else if (command === "remove") {
+  console.log("removing note");
+}
